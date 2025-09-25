68f442f46dd1b752abdfc0698b7dc97f43411e3b
David Robinson <4973064-daveed9@users.noreply.gitlab.com>
2025-09-26 00:54:24 +0200

    Added keybinds for system clipboard. Removed some notes.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -19,12 +19,6 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- Close current buffer
 vim.keymap.set('n', '<leader>Q', ':bd<CR>', { desc = 'Close current buffer' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
@@ -39,6 +33,12 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- Keybinds for system clipboard
+vim.keymap.set('n', 'Y', '"+y', { desc = 'Copy to system clipboard' })
+vim.keymap.set('n', 'D', '"+d', { desc = 'Delete and copy to system clipboard' })
+vim.keymap.set('n', '<C-p>', '"+p', { desc = 'Paste after from system clipboard' })
+vim.keymap.set('n', '<C-P>', '"+P', { desc = 'Paste before from system clipboard' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
